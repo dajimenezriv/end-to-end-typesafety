c93f164ab1f5005e90149e727d16224ddc136c98
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2024-11-03 18:56:08 +0100

    Create angular

---
+++ angular/src/core/componentes/spartan/hlm-icon/index.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { HlmIconComponent } from './hlm-icon.component';
+
+export * from './hlm-icon.component';
+
+@NgModule({
+  imports: [HlmIconComponent],
+  exports: [HlmIconComponent],
+})
+export class HlmIconModule {}
