8fa3455d81b2a0d1dad58f2915f1c3f0e742557a
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2024-11-04 19:29:58 +0100

    Create angular-pokemon

---
+++ angular-pokemon/src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'list',
+    loadComponent: () => import('./pokemons/pokemon-list/pokemon-list.component').then((m) => m.PokemonListComponent),
+    title: 'Pokemon List',
+  },
+  {
+    path: 'list/pokemon/:id',
+    loadComponent: () => import('./pokemons/pokemon/pokemon.component').then((m) => m.PokemonComponent),
+    title: 'Pokemon Details',
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/list?page=1',
+  },
+  {
+    path: '**',
+    redirectTo: '/list?page=1',
+  },
+];
