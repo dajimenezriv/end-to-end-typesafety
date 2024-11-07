97ff45d3fb789493bb936348d1d60d9fb6390093
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2024-11-07 19:56:26 +0100

    Add tailwind and shadcn

--- react-18/src/main.tsx
+++ react-18/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import { FormComponent } from './pages/form/ui/Form';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FormComponent />
   </StrictMode>,
 );
