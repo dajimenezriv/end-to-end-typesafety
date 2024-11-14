1af302078438f3e1a636f9a64660470d365832a8
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2024-11-14 19:21:23 +0100

    Trying composition API

---
+++ vue-3/src/components/PagesList.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>firstCounter: {{ firstCounter }}</p>
+  <p>secondCounter: {{ secondCounter.counter }}</p>
+  <button @click="click">Click</button>
+  <p>Number of pages: {{ pages.length }}</p>
+</template>
+
+<script setup lang="ts">
+import { useStore } from '@/store'
+import { ref, reactive, computed } from 'vue'
+
+const firstCounter = ref(0)
+const secondCounter = reactive({ counter: 0 })
+const store = useStore()
+
+const pages = computed(() => store.pages)
+
+const click = () => {
+  firstCounter.value++
+  secondCounter.counter++
+}
+</script>
--- vue-3/src/routes.ts
+++ vue-3/src/routes.ts
@@ -2,11 +2,19 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import PageViewer from './components/PageViewer.vue'
 import CreatePage from './components/CreatePage.vue'
 import AllPages from './components/AllPages.vue'
+import PagesList from './components/PagesList.vue'
 
 export const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     { path: '/:index?', component: PageViewer, props: true },
-    { path: '/pages', component: AllPages, children: [{ path: 'create', component: CreatePage }] },
+    {
+      path: '/pages',
+      component: AllPages,
+      children: [
+        { path: '', component: PagesList },
+        { path: 'create', component: CreatePage },
+      ],
+    },
   ],
 })
