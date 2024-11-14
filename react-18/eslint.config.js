67f49316f0571883e1ec9bb07bcea6614fdbc0a0
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2024-11-14 19:21:43 +0100

    Understanding eslint 9

--- react-18/eslint.config.js
+++ react-18/eslint.config.js
@@ -1,28 +1,37 @@
 import js from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import configPrettier from 'eslint-config-prettier';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import pluginPrettier from 'eslint-plugin-prettier';
+import pluginImport from 'eslint-plugin-import';
+import pluginTanstackQuery from '@tanstack/eslint-plugin-query';
 
 export default tseslint.config(
   { ignores: ['dist', 'coverage'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier, eslintPluginPrettier],
+    extends: [...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-hooks': pluginReactHooks,
+      'react-refresh': pluginReactRefresh,
+      '@tanstack/query': pluginTanstackQuery,
+      import: pluginImport,
+      prettier: pluginPrettier,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      ...js.configs.recommended.rules,
+      ...pluginReactHooks.configs.recommended.rules,
+      ...pluginTanstackQuery.configs.recommended.rules,
+      ...configPrettier.rules,
       '@typescript-eslint/no-empty-object-type': 'off',
+      'import/no-default-export': 'error',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
 );
