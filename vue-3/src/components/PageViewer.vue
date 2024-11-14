1a9a5a5a15eb0dc25014b292a829417b807ad96c
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2024-11-14 18:32:05 +0100

    Param as props

--- vue-3/src/components/PageViewer.vue
+++ vue-3/src/components/PageViewer.vue
@@ -11,28 +11,40 @@ import { useStore } from '@/store'
 import { computed } from 'vue'
 import z from 'zod'
 
-const PARAMS = z.object({ index: z.coerce.number() })
+// const PARAMS = z.object({ index: z.coerce.number() })
 
 export default {
+  props: {
+    index: {
+      type: String,
+      required: true,
+    },
+  },
   setup() {
     const store = useStore()
     const pages = computed(() => store.pages)
     return { store, pages }
   },
   created() {
-    this.$watch(
-      () => this.$route.params,
-      async (params) => {
-        const parsedParams = await PARAMS.safeParseAsync(params)
-        const index = parsedParams.data?.index ?? 0
-        this.page = this.pages[index]
-      },
-    )
+    // this.$watch(
+    //   () => this.$route.params,
+    //   async (params) => {
+    //     const parsedParams = await PARAMS.safeParseAsync(params)
+    //     const index = parsedParams.data?.index ?? 0
+    //     this.page = this.pages[index]
+    //   },
+    // )
   },
   data() {
     return {
       page: null as TPage | null,
     }
+  },
+  watch: {
+    index(index: string | string[]) {
+      const parsedIndex = z.coerce.number().safeParse(index)
+      this.page = this.pages[parsedIndex.data ?? 0]
+    },
   },
 }
 </script>
