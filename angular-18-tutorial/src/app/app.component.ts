d1d542e9e02f96c0fc75382e44ed5a1327d43b27
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2024-11-24 16:20:36 +0100

    Use components

--- angular-18-tutorial/src/app/app.component.ts
+++ angular-18-tutorial/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { WishItem } from "../shared/models/wish-item";
 import { FormsModule } from "@angular/forms";
+import { WishListComponent } from "./wish-list/wish-list.component";
+import { AddWishFormComponent } from "./add-wish-form/add-wish-form.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, WishListComponent, AddWishFormComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
@@ -16,15 +18,17 @@ export class AppComponent {
     new WishItem("Get Coffee", true),
     new WishItem("Find grass that cuts itself"),
   ];
+  listFilter: string = "0";
 
-  newWishText = "";
+  get visibleItems(): Array<WishItem> {
+    const value = this.listFilter;
 
-  addNewWish() {
-    this.items.push(new WishItem(this.newWishText));
-    this.newWishText = "";
-  }
-
-  toggleItem(item: WishItem) {
-    item.isComplete = !item.isComplete;
+    if (value === "1") {
+      return this.items.filter((item) => !item.isComplete);
+    } else if (value === "2") {
+      return this.items.filter((item) => item.isComplete);
+    } else {
+      return this.items;
+    }
   }
 }
